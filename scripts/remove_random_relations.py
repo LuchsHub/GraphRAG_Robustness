c21bbcc42c1084de7a5b2def60de8ae9021bb945
LuchsHub <RobinWagner1910.RW@gmail.com>
2026-09-17 19:21:31 +0200

    Add script for random triple deletion

---
+++ scripts/remove_random_relations.py
@@ -0,0 +1,54 @@
+import csv
+import os
+import random
+
+BASE_GRAPH = "../graphs/stark-amazon"
+DROP_RATIO = 0.50
+SEED = 7
+
+random.seed(SEED)
+
+# Find new output dir name and create
+i = 0
+while os.path.exists(f"{BASE_GRAPH}-rel-incomp-{i}"):
+    i += 1
+output_dir = f"{BASE_GRAPH}-rel-incomp-{i}"
+os.makedirs(output_dir, exist_ok=True)
+
+input_file = os.path.join(BASE_GRAPH, "triples.csv")
+output_file = os.path.join(output_dir, "triples.csv")
+
+# Deduplicate bidirectional triples
+dedup_triples = set()
+
+with open(input_file, "r", encoding="utf-8") as infile:
+    reader = csv.reader(infile)
+    header = next(reader)
+
+    for row in reader:
+        src_id = row[0]
+        rel = row[1]
+        dst_id = row[2]
+
+        sorted_triple = (src_id, rel, dst_id) if src_id < dst_id else (dst_id, rel, src_id)
+
+        if sorted_triple not in dedup_triples:
+            dedup_triples.add(sorted_triple)
+
+total_triples = len(dedup_triples)
+num_to_keep = int(total_triples * (1.0 - DROP_RATIO))
+print(total_triples)
+
+# Sampple random triples
+dedup_triples = list(dedup_triples)
+keep_triples = random.sample(dedup_triples, num_to_keep)
+print(len(keep_triples))
+
+# 4. Write to output
+with open(output_file, "w", newline="", encoding="utf-8") as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(header)
+
+    for u, rel, v in keep_triples:
+        writer.writerow([u, rel, v])
+        writer.writerow([v, rel, u])
