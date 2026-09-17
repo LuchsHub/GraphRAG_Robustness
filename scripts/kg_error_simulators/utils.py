6c0c3d54289e6e5b62605da2b81e4bebc17e25d4
LuchsHub <RobinWagner1910.RW@gmail.com>
2026-09-17 21:08:35 +0200

    Update get_deduplicated_triples_from_csv to return max_id along with deduplicated triples

--- scripts/kg_error_simulators/utils.py
+++ scripts/kg_error_simulators/utils.py
@@ -1,9 +1,10 @@
 import csv
 
 
-def get_deduplicated_triples_from_csv(input_file: str) -> tuple[list, set]:
+def get_deduplicated_triples_from_csv(input_file: str) -> tuple[list, set, int]:
     dedup_triples = set()
 
+    max_id = -1
     with open(input_file, "r", encoding="utf-8") as infile:
         reader = csv.reader(infile)
         header = next(reader)
@@ -17,7 +18,14 @@ def get_deduplicated_triples_from_csv(input_file: str) -> tuple[list, set]:
                 (src_id, rel, dst_id) if src_id < dst_id else (dst_id, rel, src_id)
             )
 
-            if sorted_triple not in dedup_triples:
-                dedup_triples.add(sorted_triple)
+            if sorted_triple in dedup_triples:
+                continue
 
-    return header, dedup_triples
+            if int(src_id) > max_id:
+                max_id = int(src_id)
+            if int(dst_id) > max_id:
+                max_id = int(dst_id)
+
+            dedup_triples.add(sorted_triple)
+
+    return header, dedup_triples, max_id
