62926314168785bb69df875604ac57af7768ebcf
LuchsHub <RobinWagner1910.RW@gmail.com>
2026-09-26 15:13:29 +0200

    Relational error simulators use multiple seeds

--- scripts/error_simulators/add_random_relations.py
+++ scripts/error_simulators/add_random_relations.py
@@ -12,16 +12,13 @@ BASE_GRAPH = "../../graphs/stark-amazon"
 with open(CONFIG_FILE, "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
-SEED = config["error_simulation"]["seed"]
-ADDITIONAL_EDGE_RATIO = config["error_simulation"]["add_random_relations"]["add_edge_ratio"]
+SEEDS = config["error_simulation"]["seeds"]
+ADDITIONAL_EDGE_RATIO = config["error_simulation"]["add_random_relations"][
+    "add_edge_ratio"
+]
 RELATION_TYPES = config["error_simulation"]["add_random_relations"]["relation_types"]
 
-random.seed(SEED)
-output_dir = f"{BASE_GRAPH}-rel-add-{SEED}"
-os.makedirs(output_dir)
-
 input_file = os.path.join(BASE_GRAPH, "triples.csv")
-output_file = os.path.join(output_dir, "triples.csv")
 
 # deduplicate bidirectional triples
 _, dedup_triples, max_id = get_deduplicated_triples_from_csv(input_file)
@@ -29,29 +26,40 @@ _, dedup_triples, max_id = get_deduplicated_triples_from_csv(input_file)
 total_triples = len(dedup_triples)
 num_to_add = int(total_triples * ADDITIONAL_EDGE_RATIO)
 
-# expand base file
-shutil.copyfile(input_file, output_file)
+for seed in SEEDS:
+    output_dir = f"{BASE_GRAPH}-rel-add-{seed}"
+    try:
+        os.makedirs(output_dir)
+    except OSError:
+        print(f"{output_dir} already exists, skipping seed.")
+        continue
+    output_file = os.path.join(output_dir, "triples.csv")
 
-with open(output_file, "a", newline="", encoding="utf-8") as outfile:
-    writer = csv.writer(outfile)
+    random.seed(seed)
 
-    # add random generic edges
-    added_triples = set()
-    while len(added_triples) < num_to_add:
-        # random nodes (can be any entity type, but mostly products since they are the majority)
-        src_id = random.randint(0, max_id)
-        dst_id = random.randint(0, max_id)
-        # no self-loops
-        while src_id == dst_id:
+    # extend existing triples file
+    shutil.copyfile(input_file, output_file)
+
+    with open(output_file, "a", newline="", encoding="utf-8") as out:
+        writer = csv.writer(out)
+
+        # add random generic edges
+        added_triples = set()
+        while len(added_triples) < num_to_add:
+            # random nodes (can be any entity type, but mostly products since they are the majority)
+            src_id = random.randint(0, max_id)
             dst_id = random.randint(0, max_id)
-        rel = random.choice(RELATION_TYPES)
+            # no self-loops
+            while src_id == dst_id:
+                dst_id = random.randint(0, max_id)
+            rel = random.choice(RELATION_TYPES)
 
-        # edges should be unique
-        triple = (src_id, rel, dst_id) if src_id < dst_id else (dst_id, rel, src_id)
-        if triple in added_triples:
-            continue
-        added_triples.add(triple)
+            # edges should be unique
+            triple = (src_id, rel, dst_id) if src_id < dst_id else (dst_id, rel, src_id)
+            if triple in added_triples:
+                continue
+            added_triples.add(triple)
 
-        # add edges bidirectionally
-        writer.writerow([src_id, rel, dst_id])
-        writer.writerow([dst_id, rel, src_id])
+            # add edges bidirectionally
+            writer.writerow([src_id, rel, dst_id])
+            writer.writerow([dst_id, rel, src_id])
--- scripts/error_simulators/remove_random_relations.py
+++ scripts/error_simulators/remove_random_relations.py
@@ -11,15 +11,10 @@ BASE_GRAPH = "../../graphs/stark-amazon"
 with open(CONFIG_FILE, "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
-SEED = config["error_simulation"]["seed"]
+SEEDS = config["error_simulation"]["seeds"]
 DROP_RATIO = config["error_simulation"]["remove_random_relations"]["drop_ratio"]
 
-random.seed(SEED)
-output_dir = f"{BASE_GRAPH}-rel-incomp-{SEED}"
-os.makedirs(output_dir)
-
 input_file = os.path.join(BASE_GRAPH, "triples.csv")
-output_file = os.path.join(output_dir, "triples.csv")
 
 # Deduplicate bidirectional triples
 header, dedup_triples, _ = get_deduplicated_triples_from_csv(input_file)
@@ -29,14 +24,25 @@ num_to_keep = int(total_triples * (1.0 - DROP_RATIO))
 
 # Sample random triples
 dedup_triples = list(dedup_triples)
-print(len(dedup_triples))
-keep_triples = random.sample(dedup_triples, num_to_keep)
 
-with open(output_file, "w", newline="", encoding="utf-8") as outfile:
-    writer = csv.writer(outfile)
-    writer.writerow(header)
+for seed in SEEDS:
+    output_dir = f"{BASE_GRAPH}-rel-incomp-{seed}"
+    try:
+        os.makedirs(output_dir)
+    except OSError:
+        print(f"{output_dir} already exists, skipping seed.")
+        continue
+    output_file = os.path.join(output_dir, "triples.csv")
 
-    # only write sampled triples
-    for u, rel, v in keep_triples:
-        writer.writerow([u, rel, v])
-        writer.writerow([v, rel, u])
+    # sample random triples
+    random.seed(seed)
+    keep_triples = random.sample(dedup_triples, num_to_keep)
+
+    with open(output_file, "w", newline="", encoding="utf-8") as out:
+        writer = csv.writer(out)
+        writer.writerow(header)
+
+        # only write sampled triples
+        for u, rel, v in keep_triples:
+            writer.writerow([u, rel, v])
+            writer.writerow([v, rel, u])
